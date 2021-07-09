8250cb667d3ef5bfaaccb7cf945a8a1cae5e21fe
SiwachetPhumbut <siwachet2059@gmail.com>
2021-07-10 05:01:22 +0700

    initial commit

---
+++ Ex2.php
@@ -0,0 +1,15 @@
+<?php
+    $number1 = 100;
+    $number2 = 20;
+
+    if($number1 > $number2){
+        echo "Max : " . $number1;
+    }else{
+        echo "Max : " . $number2;
+    }
+
+
+
+
+
+?>
---
+++ Ex4.php
@@ -0,0 +1,15 @@
+<?php
+    $number1 = 10;
+    $number2 = 100;
+
+
+    if($number1 > $number2){
+        echo  "Max : " . $number1;
+    }else if($number2 > $number1){
+        echo  "Max : " . $number2;
+    }else {
+        echo  "number1 == number2";
+    }
+
+?>
+
